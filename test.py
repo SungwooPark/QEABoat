9a2763d6ae71092fa732ec48fe539f64c207ceef
Sung Park <sungwoo.park@students.olin.edu>
2016-02-18 11:04:57 -0500

    Added center of mass function

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import dblquad
 
 def boat_plot(n,theta,d):
     plt.clf()
@@ -16,4 +17,16 @@ def boat_plot(n,theta,d):
 
     plt.show()
 
-boat_plot(4,np.pi/4,1.2)
+def com(n,density):
+    #still have to divide this thing by total mass
+    total_mass = dblquad(lambda z,y: density, -1,1, lambda y:np.absolute(y)**n-1,lambda y:0)[0]
+    return (dblquad(lambda z,y: density*y,-1,1,lambda y:np.absolute(y)**n-1,lambda y:0)[0]/total_mass,dblquad(lambda z,y: density*z,-1,1,lambda y:np.absolute(y)**n-1,lambda y:0)[0]/total_mass)
+
+#boat_plot(4,np.pi/4,1.2)
+
+#print dblquad(lambda z,y: 100*y,-1,1,lambda y:np.absolute(y)**2-1,lambda y:0)
+#print dblquad(lambda z,y: 100*z,-1,1,lambda y:np.absolute(y)**2-1,lambda y:0)
+
+print com(2,100)
+print com(3,100)
+print com(4,100)
