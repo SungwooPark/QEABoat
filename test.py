418d7a35ab12496627f5caa4529d7695f6684e05
Sung Park <sungwoo.park@students.olin.edu>
2016-02-18 11:50:26 -0500

    Added a code to find intersection points

--- test.py
+++ test.py
@@ -9,24 +9,27 @@ def boat_plot(n,theta,d):
     axes.set_xlim([-1,1])
     axes.set_ylim([-1.25, 0])
 
-    y = np.linspace(-1,1,100)
+    y = np.linspace(-1,1,100) #range of function
     f1 = np.absolute(y)**n - 1
     f2 = -np.tan(theta)* y - d
     plt.plot(y,f1)
     plt.plot(y,f2)
 
+    idx = np.argwhere(np.isclose(f1,f2, atol=0.01)).reshape(-1)
+    print f1[idx], f2[idx]
+    plt.plot(y[idx], f1[idx], 'ro')
+
     plt.show()
 
 def com(n,density):
-    #still have to divide this thing by total mass
     total_mass = dblquad(lambda z,y: density, -1,1, lambda y:np.absolute(y)**n-1,lambda y:0)[0]
     return (dblquad(lambda z,y: density*y,-1,1,lambda y:np.absolute(y)**n-1,lambda y:0)[0]/total_mass,dblquad(lambda z,y: density*z,-1,1,lambda y:np.absolute(y)**n-1,lambda y:0)[0]/total_mass)
 
-#boat_plot(4,np.pi/4,1.2)
+boat_plot(4,np.pi/4,1.2)
 
 #print dblquad(lambda z,y: 100*y,-1,1,lambda y:np.absolute(y)**2-1,lambda y:0)
 #print dblquad(lambda z,y: 100*z,-1,1,lambda y:np.absolute(y)**2-1,lambda y:0)
 
-print com(2,100)
-print com(3,100)
-print com(4,100)
+#print com(2,100)
+#print com(3,100)
+#print com(4,100)
