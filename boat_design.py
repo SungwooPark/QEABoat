0c08df09099dfa73f72a4359f96a973a24abf581
willem.thorbecke@students.olin.edu <willem.thorbecke@students.olin.edu>
2016-02-22 10:28:58 -0500

    plotting works

--- boat_design.py
+++ boat_design.py
@@ -13,7 +13,7 @@ def righting_arm(n,theta):
         Returns a coordinate and magitude of righting moment of the boat
     '''
     
-    d = 1 #compare() will replace this
+    d = .5 #compare() will replace this
 
     y = np.linspace(-1,1,1000)
     f1 = np.absolute(y)**n - 1 #Hull function without lambda    
@@ -23,18 +23,21 @@ def righting_arm(n,theta):
     water_line = lambda y: -np.tan(theta)*y - d
     
     displacement(hull_function, water_line,n,theta,d) 
-    plot_graph(f1, f2)
+    plot_graph(f1, f2,hull_function,water_line,intersection(hull_function,water_line,n,theta,d))
    
 def intersection(hull_func,water_func,n,theta,d):
     '''
         Takes in function representation of hull and water line and then returns the roots
     '''
     func_difference = lambda y: (-np.tan(theta)*y - d) - (np.absolute(y)**n - 1)
-    root = fsolve(func_difference,[-1,1])
+    root = fsolve(func_difference,[-10,10])
+    print "init_root: ", root
+
     if np.absolute(root[0]) > 1:
         root[0] = fsolve(water_func,0)
     elif np.absolute(root[1]) >1:
         root[1] = fsolve(water_func,0)
+    print 'this is root', root
     return root
 
 def displacement(hull_func, water_func,n,theta,d):
@@ -45,6 +48,7 @@ def displacement(hull_func, water_func,n,theta,d):
     limits = intersection(hull_func, water_func,n,theta,d)
     func_difference = lambda y: (-np.tan(theta)*y - d)- (np.absolute(y)**n - 1)
     
+    print "root value: ", abs(water_func(limits[0]))
     if abs(water_func(limits[0]))<0.01:
         area1 = np.absolute(integrate.quad(hull_func,-1,limits[0])[0])
         print 'area1: ',area1
@@ -53,24 +57,33 @@ def displacement(hull_func, water_func,n,theta,d):
         displacement = area1 + area2
         print 'displacement: ',displacement
     else:     
-        limits = intersection(hull_func, water_func,n,theta,d)
         result = integrate.quad(func_difference,limits[0],limits[1])
         print "displacement volume: ", result[0]
  
-def plot_graph(hull,waterline):
+def plot_graph(hull,waterline,lambda_hull,lambda_waterline,intersection_points):
     '''
         plots the graph of the hull and water line
+        args: intersection_points - a list of x coord of intersection of boat and waterline
     '''
     
     #Reset axes
     axes = plt.gca()
     axes.set_xlim([-1.7,1.7])
-    axes.set_ylim([-2,2])    
+    axes.set_ylim([-2,2]) 
+
+    root1 = intersection_points[0]
+    root2 = intersection_points[1]
 
     y = np.linspace(-1,1,1000)
     plt.plot(y,hull)
     plt.plot(y,waterline)
-    plt.plot([-1,1],[0,0])
+    plt.plot([-2,2],[0,0])
+
+    if np.absolute(lambda_waterline(root1)) < 0.01 or np.absolute(lambda_waterline(root2)) < 0.01:
+        plt.plot([root1,root2],[lambda_waterline(root1),lambda_waterline(root2)], 'ro')
+    else:
+        plt.plot([root1,root2],[lambda_hull(root1),lambda_hull(root2)], 'ro')
+    
     plt.show()
 
-righting_arm(2,np.pi/2-0.03)
+righting_arm(2, np.pi/3)
